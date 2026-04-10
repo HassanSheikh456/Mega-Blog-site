c05750dbf74a9eaf11bb74266263338c39a16bbd
HassanSheikh456 <hassan.nadeem.4012@gmail.com>
2026-04-10 15:32:38 +0500

    Reusable TypeScript select component with dynamic options, label support, and forwardRef.

---
+++ src/components/Select.tsx
@@ -0,0 +1,41 @@
+"use client";
+import React, { forwardRef, useId, SelectHTMLAttributes } from "react";
+
+type SelectOption = {
+  value: string;
+  label: string;
+};
+
+type SelectProps = {
+  label?: string;
+  options: SelectOption[];
+  className?: string;
+} & SelectHTMLAttributes<HTMLSelectElement>;
+
+const Select = forwardRef<HTMLSelectElement, SelectProps>(function Select(
+  { options, label, className = "", ...props },
+  ref,
+) {
+  const id = useId();
+  return (
+    <div className="w-full">
+      {label && <label htmlFor={id}></label>}
+      <select
+        className={`px-3 py-2 rounded-lg bg-white 
+        text-black outline-none focus:bg-gray-50 
+        duration-200 border border-gray-200  w-full ${className}`}
+        {...props}
+        ref={ref}
+        id={id}
+      >
+        {options?.map((option: SelectOption) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+});
+
+export default Select;
